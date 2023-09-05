515b194d8fca6b636bf29083b547978fe2d39b9a
tonny <anthonny14gabriell@gmail.com>
2023-09-05 14:14:40 -0400

    creating aside component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="p-4 flex justify-between">
+      <div>
+        Logo here
+      </div>
+      <div>
+        <input type="text" name="" id="" placeholder="search"/>
+      </div>
+      <nav>
+        <div>
+          user icon
+        </div>
+      </nav>
+    </header>
   )
 }
 
